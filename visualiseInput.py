fb323cf40c894b0ece76e863aa8d4d09020ed5ac
Hunsley.IO Ltd <alex.hunsley@gmail.com>
2020-03-13 11:30:17 +0000

    Fix chart generator borders

--- visualiseInput.py
+++ visualiseInput.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 #
-# visualise Input articles
+# visualise Input article distribution by generating png images.
 #
+# To run this script, you'll need to install pillow:
 #
+#   pip install pillow
+#
+
 import csv
 from PIL import Image
 from PIL import ImageDraw
@@ -18,41 +22,47 @@ xBorder = 16
 yBorder = 16
 
 
-maxPages = 50
+maxPages = 36
 
-imgWidth = xBorder * 2  + 51 * (barWidth + 1)
+imgWidth = xBorder * 2  + 51 * barWidth
 imgHeight = yBorder * 2 + onePageThickness * maxPages
 
 
-font = ImageFont.truetype("Futura.ttc", 25)
+font = ImageFont.truetype("Futura.ttc", 21)
 
 articleColours = { 'bp' : '#990D7D', 'gp' : '#FF5D00', 'mc' : '#1B5FD1', 'l' : '#F7280D', 'p' : '#A2CA20', 'a' : '#FFCE00', 'none' : 'lightgrey'}
-categoryNames = { 'bp' : "Basic Programming", 'gp' : "Graphics Programming", 'mc' : "Machine Code", 'l' : "Languages", 'p' : "Peripherals", 'a' : "Applications"}
+categoryNames = { 'bp' : "Basic Programming", 'gp' : "Graphics Programming", 'mc' : "Machine Code", 'l' : "Languages", 'p' : "Peripherals", 'a' : "Applications", 'all' : 'All articles', 'none' : 'No highlights'}
 
 
 def produceChartForCode(articleCode='all'):
 	img = Image.new('RGB', (imgWidth, imgHeight), color = 'white')
 	draw = ImageDraw.Draw(img, 'RGB')
 	
-	x = xBorder
+	x = xBorder - barWidth
 	y = yBorder
 
 	with open('Input magazine content tagging.csv', 'r') as csvfile:
 		spamreader = csv.reader(csvfile, delimiter=',', quotechar='"')
 
-		idx = 0
+		next(spamreader)
+		next(spamreader)
+		next(spamreader)
+		
+		# idx = 0
 
 		currentIssueNumber = 0
 		articlePageOffsetInCurrentIssue = 0
 
 		for row in spamreader:
-			idx += 1
-			if (idx < 4):
-				continue
+			print("___ Doing a row, x y = ", x, y, " row = ", row)
+			# idx += 1
+			# if (idx < 4):
+			# 	continue
 
 			articleNumber = row[0]
 			issueNumber = row[1]
 
+			print("got issue, article = ", issueNumber, articleNumber)
 			# stop at the index article
 			if (articleNumber == "270"):
 				break
@@ -76,12 +86,13 @@ def produceChartForCode(articleCode='all'):
 			# if (series != 'gp'):
 				series = 'none'
 
+			print("Drawing bar at ", x, y)
 			draw.rectangle([x, y,  x + barWidth - 2, y + barHeight - 2], fill=articleColours[series])
 
 			y += barHeight
 
 	# text for category name
-	draw.text((50, imgHeight - 50), "Article code: %s" % articleCode, fill=(0, 0, 0, 255), font=font)
+	draw.text((xBorder + 10, imgHeight - 42), "Articles: %s (%s)" % (categoryNames[articleCode], articleCode), fill=(0, 0, 0, 255), font=font)
 
 	img.save('inputChart-%s.png' % articleCode)		
 
